b1dbc843fcc1daba2102fbab45df5d36712fa4ad
Mostafa abdelaty <mostafa.m.abdelaty@gmail.com>
2026-05-14 19:39:12 +0300

    Debug: show real error message

--- back/server.js
+++ back/server.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ success: false, message: 'Something went wrong!' });
+  res.status(500).json({ success: false, message: err.message || 'Something went wrong!' });
 });
 
 const PORT = process.env.PORT || 5000;
